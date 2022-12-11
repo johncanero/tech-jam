f8cd53623dde3c1d7a9bc193924ad45059f5f3a9
John Canero <johncanero.official@gmail.com>
2022-12-11 18:19:25 +0800

    Update and Hero in the Content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import Head from "next/head";
-import Image from "next/image";
+import React from "react";
+import Hero from "../sections/Hero";
 
 export default function Home() {
   return (
     <div>
-      <h1 className=" text-green-400 font-bold text-6xl text-center mt-10">Tech Jam</h1>
+      {/* Main Content */}
+      <Hero />
     </div>
   );
 }
--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div>
+      <h1 className="mt-10 text-6xl font-bold text-center text-green-400 ">
+        Tech Jam
+      </h1>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
