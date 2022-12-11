68607b6461b23a481dd06ae645fd700c286d93ca
John Canero <johncanero.official@gmail.com>
2022-12-11 13:05:12 +0800

    Modify the Tailwind CSS Starter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1 className=' text-green-300'>Tech Jam</h1>
+      <h1 className=" text-green-400 font-bold text-6xl text-center mt-10">Tech Jam</h1>
     </div>
-  )
+  );
 }
