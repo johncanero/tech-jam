41acc2aeaa919a86eb8407a564743956cd9f2624
John Canero <johncanero.official@gmail.com>
2022-12-11 18:18:35 +0800

    Add Sections, Components and Modify App.js and Layout.js

---
+++ components/Layout.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Head from "next/head";
+import Header from "../sections/Header";
+import Footer from "../sections/Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      {/* Head */}
+      <Head>
+        <title>Tech Jam</title>
+        <meta
+          name="description"
+          content="Tech Jam (Technology, Products and Marketplace)"
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="Tech Jam (Technology, Products and Marketplace)" />
+        <meta property="og:description" content="Technology, Marketplace, Products, Innovation, Advices, Tips" />
+        {/* Flaticon */}
+        <link rel="shortcut icon" href="/images/lightbulb5.png" />
+      </Head>
+
+      {/* Header, Main & Footer */}
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        {/* Mobile Visibility */}
+        <main className="flex-grow">{children}</main>
+        <Footer />
+        {/* <MobileFooterButtons /> */}
+      </div>
+     
+    </>
+  );
+};
+export default Layout;
