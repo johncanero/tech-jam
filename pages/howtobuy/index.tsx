b3eff2d2e65c0590fd28dea3eb7508f8f860983e
John Canero <johncanero.official@gmail.com>
2022-12-12 14:23:56 +0800

    Change the Margin Top of the Headers

--- pages/howtobuy/index.tsx
+++ pages/howtobuy/index.tsx
@@ -5,7 +5,7 @@ const index = () => {
   return (
     <div>
       <div className="ml-6">
-        <h1 className="mt-6 text-2xl font-bold text-center md:text-left md:mt-1 md:text-3xl font-ShareTechMono dark:text-green-400">
+        <h1 className="mt-6 text-2xl font-bold text-center md:text-left md:text-3xl font-ShareTechMono dark:text-green-400">
           How To Buy
         </h1>
         <div className="max-w-4xl mt-2 mb-12">
