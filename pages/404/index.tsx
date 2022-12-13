b1c9975a19187a0000615cd595813feef469de92
John Canero <johncanero.official@gmail.com>
2022-12-13 17:29:36 +0800

    Modify 404 Content

--- pages/404/index.tsx
+++ pages/404/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const index = () => {
   return (
-    <div className="ml-6">
-      <h1 className="mt-1 text-3xl font-bold tracking-widest font-ShareTechMono dark:text-green-400">
+    <div className="mt-6">
+      <h1 className="mt-1 text-3xl font-bold tracking-widest text-center font-ShareTechMono dark:text-green-400">
         Oops! 404 Error: Page not found!😢
       </h1>
     </div>
