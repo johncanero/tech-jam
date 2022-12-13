69cf744a21cd28276ade8e9e72186cf2484c280f
John Canero <johncanero.official@gmail.com>
2022-12-13 08:49:46 +0800

    Make the Data into Component

---
+++ pages/DataComponent.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import Image from "next/image";
+import { useState } from "react";
+
+const data = [
+  {
+    id: 1,
+    mainImage: "/images/image-product-1.jpg",
+    thumbnail: "/images/image-product-1-thumbnail.jpg",
+  },
+  {
+    id: 2,
+    mainImage: "/images/image-product-2.jpg",
+    thumbnail: "/images/image-product-2-thumbnail.jpg",
+  },
+  {
+    id: 3,
+    mainImage: "/images/image-product-3.jpg",
+    thumbnail: "/images/image-product-3-thumbnail.jpg",
+  },
+  {
+    id: 4,
+    mainImage: "/images/image-product-4.jpg",
+    thumbnail: "/images/image-product-4-thumbnail.jpg",
+  },
+  {
+    id: 5,
+    mainImage: "/images/image-product-5.jpg",
+    thumbnail: "/images/image-product-5-thumbnail.jpg",
+  },
+  {
+    id: 6,
+    mainImage: "/images/image-product-6.jpg",
+    thumbnail: "/images/image-product-6-thumbnail.jpg",
+  },
+  {
+    id: 7,
+    mainImage: "/images/image-product-7.jpg",
+    thumbnail: "/images/image-product-7-thumbnail.jpg",
+  },
+];
+
+const DataComponent = () => {
+  const [products] = useState(data);
+  const [value, setValue] = useState(0);
+
+  const { mainImage } = products[value];
+  return (
+    <React.Fragment>
+      <article>
+        <Image src={mainImage} width={550} height={200} alt="" />
+
+        {/* Products Map */}
+        <div>
+          <ul className="flex-wrap items-center justify-start gap-5 mt-5 md:flex">
+            {products.map((item, index) => (
+              <li key={item.id} onClick={() => setValue(index)}>
+                <Image
+                  src={item.thumbnail}
+                  width={100}
+                  height={200}
+                  alt=""
+                  className="w-28"
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </article>
+    </React.Fragment>
+  );
+};
+
+export default DataComponent;
