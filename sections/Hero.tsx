889738f4f39f4435b0ecab95c9f1b837bfcaa841
John Canero <johncanero.official@gmail.com>
2022-12-13 19:43:08 +0800

    Add Link in Button to MX Keys

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import Link from "next/link";
 // Data
 import DataComponent from "../pages/DataComponent";
 // Icons
@@ -74,9 +75,15 @@ const Hero = () => {
 
             {/* Shopping Cart */}
             <div className="lg:flex-1">
-              <button className="flex items-center justify-center w-full gap-4 px-4 py-2 mt-3 font-bold text-white transition-all duration-200 bg-green-500 rounded-lg shadow lg:mt-0 hover:bg-green-600">
-                <AiOutlineShoppingCart /> Add to cart
-              </button>
+              <Link
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.logitech.com/en-us/products/keyboards/mx-keys-wireless-keyboard.920-009294.html"
+              >
+                <button className="flex items-center justify-center w-full gap-4 px-4 py-2 mt-3 font-bold text-white transition-all duration-200 bg-green-500 rounded-lg shadow lg:mt-0 hover:bg-green-600">
+                  <AiOutlineShoppingCart /> Add to cart
+                </button>
+              </Link>
             </div>
           </div>
         </article>
