081a34c1da477a142d2030815c5f138bf7ddfffc
John Canero <johncanero.official@gmail.com>
2022-12-13 19:47:52 +0800

    Add LinkedIn Profile at Footer Name

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     // Hide in Mobile
     <footer className="px-4 text-center text-gray-500 md:block md:py-6 font-ShareTechMono">
-      <span className="mr-2 text-lg font-bold text-gray-900 dark:text-green-400">
-        John Cañero
-      </span>
+      <Link
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.linkedin.com/in/johncanero/"
+      >
+        <span className="mr-2 text-lg font-bold text-gray-900 dark:text-green-400">
+          John Cañero
+        </span>
+      </Link>
       &copy; {new Date().getFullYear()} All Rights Reserved
     </footer>
   );
