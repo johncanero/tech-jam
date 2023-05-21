f6b089b07058ac96fc49e0673a8fdf7f3e122edf
John Canero <johncanero.official@gmail.com>
2023-05-22 07:09:00 +0800

    Change Scale of CgProfile and Image

--- components/Login.tsx
+++ components/Login.tsx
@@ -15,7 +15,7 @@ const Login: React.FC<Props> = ({ }) => {
                     onClick={(() => signOut())}
                     src={session.user?.image!}
                     alt='user-avatar'
-                    className='w-9 h-9 mx-auto lg:mt-2 mb-2 lg:mr-3 mr-3 md:mr-5 rounded-full cursor-pointer 2xl:h-12 2xl:w-12 hover:opacity-50'
+                    className='w-8 lg:w-9 mx-auto lg:mt-3 mb-2 lg:mr-3 mr-3 md:mr-5 rounded-full cursor-pointer 2xl:h-12 2xl:w-12 hover:opacity-50'
                 />
             ) : (
                 // Render the login button
@@ -23,7 +23,8 @@ const Login: React.FC<Props> = ({ }) => {
                     e.preventDefault();
                     signIn('google');
                 }}>
-                    <CgProfile size={32} className="lg:mr-3 lg:mt-3 md:mr-4 mr-3" />
+                    <CgProfile size={32} className="hidden lg:block lg:mr-3 lg:mt-3" />
+                    <CgProfile size={26} className="lg:hidden md:mr-4 mr-3 mt-1" />
                 </button>
             )}
         </div>
